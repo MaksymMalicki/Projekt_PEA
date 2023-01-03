3ea6e5435809d9788f7b9f3f1e4c06a3fb95d08c
Maksym Malicki <maksym.malicki.2115@gmail.com>
2023-01-03 13:53:16 +0100

    feat: add code

--- cmake-build-release/cmake_install.cmake
+++ cmake-build-release/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/maksymmalicki/dev/studies/PEA/project1
+# Install script for directory: /Users/maksymmalicki/dev/studies/PEA/Project_PEA
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/maksymmalicki/dev/studies/PEA/project1/cmake-build-release/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/maksymmalicki/dev/studies/PEA/Project_PEA/cmake-build-release/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
